21fbd2f927464d823de9694f60002b9765c00ede
gelbwong <karinawong04@gmail.com>
2024-07-10 21:32:15 +1000

    Added SearchEngine Component
    -Added basic Search Engine Function

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchEngine() {
+  let [searchInput, letSearchInput] = useState("");
+
+  function handleSearchInput(value) {
+    letSearchInput(value.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`furiously searching for ${searchInput} ...`);
+  }
+
   return (
-    <form>
-      <input type="search" />
+    <form onSubmit={search}>
+      <input type="search" onChange={handleSearchInput} />
       <input type="submit" value="Search" />
     </form>
   );
