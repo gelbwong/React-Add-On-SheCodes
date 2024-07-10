c8f48d1340bc7005abb4079512ab3e63acdd8d6f
gelbwong <karinawong04@gmail.com>
2024-07-10 21:11:00 +1000

    Initial Commit
    -Created Blank React-App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import SearchEngine from "./SearchEngine";
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header">
+        <h1>WASSSS UPPPPPP</h1>
+      </header>
+      <SearchEngine />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SearchEngine.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SearchEngine() {
+  return (
+    <form>
+      <input type="search" />
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
